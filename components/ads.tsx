9e0cd052559432465a9c13781709477db0b87866
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 09:19:20 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,9 +1,16 @@
 import Script from 'next/script';
-
-const AdScript = () => (
-  <Script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></Script>
+const AdScript = () => {
+  useEffect(() => {
+<Script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></Script>
 <Ins id="1052552" data-width="300" data-height="112"></Ins>
 <Script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});</Script>
-);
+    return () => {
+      // Clean up and remove the ad script
+      // ...
+    };
+  }, []);
+
+  return null;
+};
 
 export default AdScript;
