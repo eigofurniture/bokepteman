b2c21f5ae7feb6efe77df92ca4b9329e1478b24a
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 08:32:31 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -216,9 +216,9 @@ export default async function Video({ params }: PageProps) {
                     </div>
                 </CardContent>
             </Card>
-<Script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></Script>
+<Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></Script>
 <Ins id="1052552" data-width="300" data-height="112"></Ins>
-<Script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});</Script>
+<Script data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});</Script>
 
             <h2 className="text-2xl font-bold text-center my-4">
                 Related Video {file.title}
