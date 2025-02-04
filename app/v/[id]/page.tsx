64b0a3b2328bfcdd8b7b68ab87567c8cc6b5f772
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 10:40:07 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -11,7 +11,7 @@ import { Metadata, ResolvingMetadata } from "next";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { humanDuration, humanSize } from "@/lib/utils";
 import Script from "next/script";
-import AdCode from "@/components/ads";
+import GoogleAd from "@/components/ads";
 
 import { Button } from "@/components/ui/button";
 import CopyButton from "@/components/copy-button";
@@ -217,7 +217,7 @@ export default async function Video({ params }: PageProps) {
                 </CardContent>
             </Card>
 <Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js" crossOrigin="anonymous" strategy="lazyOnload"/>
-            <AdCode/>
+<GoogleAd/>
             <h2 className="text-2xl font-bold text-center my-4">
                 Related Video {file.title}
             </h2>
