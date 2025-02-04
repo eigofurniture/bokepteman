c760c165128a2d83ae38daac29f43760636460bd
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 12:49:26 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,20 +1,38 @@
-"use client";
-import {useEffect} from 'react';
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
-const GoogleAd() {
-  useEffect(() => {
-    try {
-      // @ts-ignore
-      (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});
-    } catch (err) {
-      console.error(err);
+class AdCodeWithoutRouter extends React.Component {
+  renderAds() {
+    (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});
+  }
+
+  componentDidMount() {
+    this.renderAds();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.router.asPath !== prevProps.router.asPath) {
+      this.renderAds();
     }
-  }, []);
+  }
 
-  return (
-    <ins
-      id="1052552" data-width="300" data-height="112"
-    />
-  );
+  render() {
+    return (
+      <div className="container mx-auto text-center" aria-hidden={true}>
+        <Ins
+          className="adsbyjuicy"
+          id="1052552" data-width="300" data-height="112"
+        ></Ins>
+        <Script dangerouslySetInnerHTML={{ __html: '(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});' }}></Script>
+      </div>
+    );
+  }
 }
-export default GoogleAd;
+
+const AdCode = () => {
+  const router = useRouter();
+  return <AdCodeWithoutRouter router={router} />;
+};
+
+export default AdCode;
