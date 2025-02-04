7f00c4eaf3dfadfcb5b0055a6adb5081c953700d
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 10:16:20 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,8 +1,37 @@
-import Script from 'next/script';
-const AdScript = () => (
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
-  dangerouslySetInnerHTML={{
-         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});`}}
-);
+class AdCodeWithoutRouter extends React.Component {
+  renderAds() {
+    (adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});
+  }
 
-export default AdScript;
+  componentDidMount() {
+    this.renderAds();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.router.asPath !== prevProps.router.asPath) {
+      this.renderAds();
+    }
+  }
+
+  render() {
+    return (
+      <div className="container mx-auto text-center" aria-hidden={true}>
+        <ins
+          classname="adsbyjuicy" id="1052552" data-width="300" data-height="112"
+        ></ins>
+        <script dangerouslySetInnerHTML={{ __html: '(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1052552});' }}></script>
+      </div>
+    );
+  }
+}
+
+const AdCode = () => {
+  const router = useRouter();
+  return <AdCodeWithoutRouter router={router} />;
+};
+
+export default AdCode;
